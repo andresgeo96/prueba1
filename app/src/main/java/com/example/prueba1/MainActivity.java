65a3d5b50ea305a8c61169ac34c2cd576af6610a
LAPTOP-LMHF8BQL\andres96 <andres.coro@epn.edu.ec>
2019-11-02 08:48:54 -0500

    Implementacion del ejercicio 15

---
+++ app/src/main/java/com/example/prueba1/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.prueba1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void ingEjer15(View view){
+        Intent i = new Intent(this,ejercicio15.class);
+        startActivity(i);
+    }
+
+}
