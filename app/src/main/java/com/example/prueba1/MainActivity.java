3e627e0ce4f689f911bd71444453fd8cc2f5b66d
LAPTOP-LMHF8BQL\andres96 <andres.coro@epn.edu.ec>
2019-11-03 13:31:08 -0500

    Implementacion de logion, splashcreen y un menu

--- app/src/main/java/com/example/prueba1/MainActivity.java
+++ app/src/main/java/com/example/prueba1/MainActivity.java
@@ -5,58 +5,46 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private EditText usuario;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        usuario = (EditText)findViewById(R.id.editText3);
+        password = (EditText)findViewById(R.id.editText4);
     }
 
-    public void ingEjer14P1(View view){
-        Intent i = new Intent(this,ejercicio14P1.class);
-        startActivity(i);
+    public void validar(View view) {
+        try {
+
+            String usu = usuario.getText().toString();
+            String pass = password.getText().toString();
+
+            if(usu.equals("admin")&&pass.equals("admin")){
+            Intent i = new Intent(this,menu.class);
+            i.putExtra("nombre",usuario.getText().toString());
+            startActivity(i);
+
+            }else{
+                Toast t = Toast.makeText(this, "USUARIO O CONTRASEÑA INCORRECTA", Toast.LENGTH_SHORT);
+                t.show();
+            }
+
+        } catch (Exception e) {
+            Toast t = Toast.makeText(this, "ERROR", Toast.LENGTH_SHORT);
+            t.show();
+        }
+
     }
 
-    public void ingEjer14P2(View view){
-        Intent i = new Intent(this,ejercicio14P2.class);
-        startActivity(i);
-    }
-
-    public void ingEjer15(View view){
-        Intent i = new Intent(this,ejercicio15.class);
-        startActivity(i);
-    }
-
-    public void ingEjer23(View view){
-        Intent i = new Intent(this,ejercicio23.class);
-        startActivity(i);
-    }
-
-    public void ingEjer26(View view){
-        Intent i = new Intent(this,ejercicio26.class);
-        startActivity(i);
-    }
-
-    public void ingEjer34(View view){
-        Intent i = new Intent(this,ejercicio34.class);
-        startActivity(i);
-    }
-
-    public void ingEjer39(View view){
-        Intent i = new Intent(this,ejercicio39.class);
-        startActivity(i);
-    }
-
-    public void ingEjer48(View view){
-        Intent i = new Intent(this,ejercicio48.class);
-        startActivity(i);
-    }
-
-    public void ingEjer51(View view){
-        Intent i = new Intent(this,ejercicio51.class);
-        startActivity(i);
-    }
 
 }
---
+++ app/src/main/java/com/example/prueba1/menu.java
@@ -0,0 +1,100 @@
+package com.example.prueba1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class menu extends AppCompatActivity {
+
+    private TextView tv1;
+    private TextView tv2;
+    private ListView lv1;
+
+    public String nombres [] = {"EJERCICIO 14 PARTE 1", "EJERCICIO 14 PARTE 2", "EJERCICIO 15", "EJERCICIO 23",
+            "EJERCICIO 26", "EJERCICIO 39", "EJERCICIO 48", "EJERCICIO 51"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        tv1 = (TextView)findViewById(R.id.tv1);
+        tv2 = (TextView)findViewById(R.id.usu);
+        lv1 = (ListView)findViewById(R.id.lv1);
+
+        Bundle bundle = getIntent().getExtras();
+        tv2.setText(bundle.getString("nombre"));
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item_geekipedia, nombres);
+        lv1.setAdapter(adapter);
+
+        lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(lv1.getItemIdAtPosition(position)==0){
+                    LlamarEjer41_1();
+                }else if(lv1.getItemIdAtPosition(position)==1){
+                    LlamarEjer41_2();
+                }else if(lv1.getItemIdAtPosition(position)==2){
+                    LlamarEjer15();
+                }else if(lv1.getItemIdAtPosition(position)==3){
+                    LlamarEjer23();
+                }else if(lv1.getItemIdAtPosition(position)==4){
+                    LlamarEjer26();
+                }else if(lv1.getItemIdAtPosition(position)==5){
+                    LlamarEjer34();
+                }else if(lv1.getItemIdAtPosition(position)==6){
+                    LlamarEjer48();
+                }else if(lv1.getItemIdAtPosition(position)==7){
+                    LlamarEjer51();
+                }else{
+
+                }
+            }
+        });
+
+    }
+
+    public void LlamarEjer41_1(){
+        Intent llamar = new Intent(this,ejercicio14P1.class);
+        startActivity(llamar);
+    }
+    public void LlamarEjer41_2(){
+        Intent llamar = new Intent(this,ejercicio14P2.class);
+        startActivity(llamar);
+    }
+    public void LlamarEjer15(){
+        Intent llamar = new Intent(this,ejercicio15.class);
+        startActivity(llamar);
+    }
+    public void LlamarEjer23(){
+        Intent llamar = new Intent(this,ejercicio23.class);
+        startActivity(llamar);
+    }
+    public void LlamarEjer26(){
+        Intent llamar = new Intent(this,ejercicio26.class);
+        startActivity(llamar);
+    }
+    public void LlamarEjer34(){
+        Intent llamar = new Intent(this,ejercicio34.class);
+        startActivity(llamar);
+    }
+    public void LlamarEjer39(){
+        Intent llamar = new Intent(this,ejercicio39.class);
+        startActivity(llamar);
+    }
+    public void LlamarEjer48(){
+        Intent llamar = new Intent(this,ejercicio48.class);
+        startActivity(llamar);
+    }
+    public void LlamarEjer51(){
+        Intent llamar = new Intent(this,ejercicio51.class);
+        startActivity(llamar);
+    }
+}
